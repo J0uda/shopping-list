fd9bb352b0cd88b1dd366573909cb6dc482813f8
Amine Jaoued <amine.jaoued123@gmail.com>
2023-03-29 23:40:38 +0200

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearButton = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e) {
     e.preventDefault();
@@ -20,25 +22,76 @@ function addItem(e) {
     const button = createButton("remove-item btn-link text-red");
     li.appendChild(button)
 
-    itemList.appendChild(li)
+    // Add li to the DOM
+    itemList.appendChild(li);
+
+    checkUI()
 
     itemInput.value = ""
 }
 
-    function createButton(classes) {
-        const button = document.createElement('button');
-        button.className = classes
-        const icon = createIcon('fa-solid fa-xmark')
-        button.appendChild(icon)
-        return button
+function createButton(classes) {
+    const button = document.createElement('button');
+    button.className = classes
+    const icon = createIcon('fa-solid fa-xmark')
+    button.appendChild(icon)
+    return button
+}
+
+function createIcon(classes) {
+    const icon = document.createElement('i')
+    icon.className = classes
+    return icon
+}
+
+function removeItem(e) {
+    if (e.target.parentElement.classList.contains('remove-item')){
+        if (confirm('Are you sure ?')){
+            e.target.parentElement.parentElement.remove();
+            
+            checkUI()
+        }
+    }
+}
+
+function clearItem() {
+    while(itemList.firstChild) {
+        itemList.removeChild(itemList.firstChild)
     }
 
-    function createIcon(classes) {
-        const icon = document.createElement('i')
-        icon.className = classes
-        return icon
+    checkUI()
+}
+
+function filterItem(e) {
+    const items = itemList.querySelectorAll("li");
+    const text = e.target.value.toLowerCase();
+
+    items.forEach(item => {
+        const itemName = item.firstChild.textContent.toLowerCase()
+        
+        if (itemName.indexOf(text) != -1) {
+            item.style.display = 'flex'
+        } else item.style.display = "none"
+    })
+}
+
+function checkUI() {
+    const items = itemList.querySelectorAll("li");
+
+    if (items.length === 0) {
+        clearButton.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        clearButton.style.display = 'block';
+        itemFilter.style.display = 'block';
     }
+} 
+
 
 // Event Listener 
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearItem);
+itemFilter.addEventListener('input', filterItem);
 
+checkUI()
