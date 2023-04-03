5f9b2de53980a7ecde8b5f1b878fc4be60b2d42d
Amine Jaoued <amine.jaoued123@gmail.com>
2023-04-03 22:48:10 +0200

    Second commit

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
-
+const formBtn = itemForm.querySelector('button')
+let isEditMode = false
 
 function displayItems() {
     const itemsFromStorage = getItemsFromStorage()
@@ -21,6 +22,16 @@ function onItemSubmit(e) {
     if (newItem === "") {
         alert('Please add an item');
         return;
+    }
+
+    // Check for edit mode
+    if (isEditMode) {
+        const itemToEdit = itemList.querySelector('.edit-mode');
+
+        removeItemFromStorage(itemToEdit.textContent)
+        itemToEdit.classList.remove('edit-mode');
+        itemToEdit.remove();
+        isEditMode = false
     }
 
     // Create item DOM element
@@ -87,7 +98,24 @@ function createIcon(classes) {
 function onClickItem(e) {
     if (e.target.parentElement.classList.contains('remove-item')){
         removeItem(e.target.parentElement.parentElement);
+    } else {
+        setItemToEdit(e.target)
     }
+}
+
+function setItemToEdit(item) {
+    isEditMode = true
+
+    itemList
+        .querySelectorAll('li')
+        .forEach(i => i.classList.remove('edit-mode'))
+
+    item.classList.add('edit-mode');
+    formBtn.innerHTML = '<i class="fa-solid fa-pen"></i>   Update Item';
+    formBtn.style.backgroundColor = '#228822'
+    itemInput.value = item.textContent;
+
+
 }
 
 function removeItem(item) {
@@ -119,6 +147,7 @@ function clearItem() {
 
     // Clear from local Storage
     localStorage.removeItem('items');
+
     checkUI()
 }
 
@@ -136,6 +165,8 @@ function filterItem(e) {
 }
 
 function checkUI() {
+    itemInput.value = ''
+
     const items = itemList.querySelectorAll("li");
 
     if (items.length === 0) {
@@ -145,6 +176,12 @@ function checkUI() {
         clearButton.style.display = 'block';
         itemFilter.style.display = 'block';
     }
+
+
+    formBtn.innerHTML = '<i class="fa-solid fa-plus"></i>  Add Item';
+    formBtn.style.backgroundColor = '#333'
+    
+    isEditMode = false
 } 
 
 // Initialize App 
